3d31d60e8c411d70ad6899ec5932e61b5f18b84f
nathan hains <nathanhains14@yahoo.com>
2021-02-18 17:15:17 -0500

    commit tree 3a7f9cdb012fd9581940fc2152ef05d022905bd3

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  attr_accessor :side_1, :side_2, :side_3
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind 
+    if self.side_1 + self.side_2 < self.side_3 || self.side_2 + self.side_3 < self.side_1 || self.side_1 + self.side_3 < self.side_2
+      raise TriangleError
+    elsif self.side_1 + self.side_2 == self.side_3 || self.side_2 + self.side_3 == self.side_1 || self.side_1 + self.side_3 == self.side_2
+      raise TriangleError
+    elsif self.side_1 == 0 || self.side_2 == 0 || self.side_2 == 0 
+      raise TriangleError
+    elsif self.side_1 == self.side_2 && self.side_2 == self.side_3 && self.side_3 == self.side_1
+      return :equilateral
+    elsif self.side_1 == self.side_2 || self.side_2 == self.side_3 || self.side_3 == self.side_1
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
+    class TriangleError < StandardError
+
+    end
 end
